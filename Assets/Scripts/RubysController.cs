b5fbad31521e5e9c293d654220eec1de5eef950f
Alexthipp <83775919+Alexthipp@users.noreply.github.com>
2021-09-28 16:33:16 +0200

    del 1 al 7 en la tarjeta El control del personaje desde el teclado

--- Assets/Scripts/RubysController.cs
+++ Assets/Scripts/RubysController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyController : MonoBehaviour
+public class RubysController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,8 +13,10 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
 }
